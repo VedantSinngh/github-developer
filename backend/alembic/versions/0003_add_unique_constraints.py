c9b1e0932cb21dd1e7d430133513d074ae379b19
VedantSinngh <vedaantsinngh@gmail.com>
2026-07-27 15:19:29 +0530

    fix: correct mismatched down_revision ID in alembic migration

--- backend/alembic/versions/0003_add_unique_constraints.py
+++ backend/alembic/versions/0003_add_unique_constraints.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '0003'
-down_revision: Union[str, None] = '0002'
+down_revision: Union[str, None] = '0002_add_share_token'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
